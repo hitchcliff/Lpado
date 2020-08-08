abbb1414be9d02d82ccc54214e79bed18df5bc1c
Kevin Nacario <hitchcliff1@gmail.com>
2020-08-09 01:26:57 +0800

    Updates

---
+++ precache-manifest.b942147b78e5a9f29faa120c785f24a6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285876393a43d3698a6e5cff9521ccb2",
+    "url": "/hitchcliff/Lpado/index.html"
+  },
+  {
+    "revision": "2b476029e5c0a6de67e8",
+    "url": "/hitchcliff/Lpado/static/css/2.52a76f44.chunk.css"
+  },
+  {
+    "revision": "8e0cb10ee5ed1152bdde",
+    "url": "/hitchcliff/Lpado/static/css/main.971d85e5.chunk.css"
+  },
+  {
+    "revision": "2b476029e5c0a6de67e8",
+    "url": "/hitchcliff/Lpado/static/js/2.e19bf6aa.chunk.js"
+  },
+  {
+    "revision": "47ae94274444be7d9dbb5ec0082e134a",
+    "url": "/hitchcliff/Lpado/static/js/2.e19bf6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0cb10ee5ed1152bdde",
+    "url": "/hitchcliff/Lpado/static/js/main.c9d18531.chunk.js"
+  },
+  {
+    "revision": "bb0b83ec6b09fd1c3fe4",
+    "url": "/hitchcliff/Lpado/static/js/runtime-main.5e7184f1.js"
+  },
+  {
+    "revision": "b735719b00e6a3f8051fe470c8d35155",
+    "url": "/hitchcliff/Lpado/static/media/left.b735719b.svg"
+  },
+  {
+    "revision": "b5fede89190d175115a24f87c17c930d",
+    "url": "/hitchcliff/Lpado/static/media/model1.b5fede89.png"
+  },
+  {
+    "revision": "66e0a96cf5b5deece4fb1df7db28a4cb",
+    "url": "/hitchcliff/Lpado/static/media/model2.66e0a96c.jpg"
+  },
+  {
+    "revision": "a50bc4fa86c4f6bdef00656e00ce959a",
+    "url": "/hitchcliff/Lpado/static/media/right.a50bc4fa.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/hitchcliff/Lpado/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/hitchcliff/Lpado/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/hitchcliff/Lpado/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/hitchcliff/Lpado/static/media/slick.f97e3bbf.svg"
+  }
+]);
