a65c3f144292abdf224164e82e15debd4a999ab1
Kevin Nacario <hitchcliff1@gmail.com>
2020-08-09 12:01:13 +0800

    Updates

---
+++ precache-manifest.d9dc45e06105221f46bea2513a4371ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f910e3d39d71499bfe33bef7c8641bdf",
+    "url": "/lpado/index.html"
+  },
+  {
+    "revision": "d8979b37bd9f2602bf60",
+    "url": "/lpado/static/css/main.44aeb605.chunk.css"
+  },
+  {
+    "revision": "8071ed57bbbff6d9ad71",
+    "url": "/lpado/static/js/2.32656bf0.chunk.js"
+  },
+  {
+    "revision": "05459a3c6db4c453e7d0d7b89b9cc7f1",
+    "url": "/lpado/static/js/2.32656bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8979b37bd9f2602bf60",
+    "url": "/lpado/static/js/main.16a5e59f.chunk.js"
+  },
+  {
+    "revision": "03fe3fe27301d501adfc",
+    "url": "/lpado/static/js/runtime-main.87457ba0.js"
+  },
+  {
+    "revision": "b735719b00e6a3f8051fe470c8d35155",
+    "url": "/lpado/static/media/left.b735719b.svg"
+  },
+  {
+    "revision": "b5fede89190d175115a24f87c17c930d",
+    "url": "/lpado/static/media/model1.b5fede89.png"
+  },
+  {
+    "revision": "a50bc4fa86c4f6bdef00656e00ce959a",
+    "url": "/lpado/static/media/right.a50bc4fa.svg"
+  }
+]);
